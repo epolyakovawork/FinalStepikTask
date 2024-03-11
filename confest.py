61ec8a50ff9bebdd991e189c678997c0cfe8aa1b
epolyakovawork <152863987+epolyakovawork@users.noreply.github.com>
2024-03-11 22:29:54 +0300

    Update confest.py
    add options language

--- confest.py
+++ confest.py
@@ -1,6 +1,11 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+browser = webdriver.Chrome(options=options)
 
 @pytest.fixture(scope="function")
 def browser():
